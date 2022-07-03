a1be0d97105432f7140c3a28bf3c7442b44b9098
bhris <contact@bhris.digital>
2022-07-02 23:48:34 -0400

    feat: New user profile page is functional
    When a new user is created, we only have their email address and auto-generated id.
    
    The new user profile form lets us get that data from the user after they sign up.
    
    What had to be done:
    - Create a form using react-hook-form with validation
    - Create an API that links to the backend and pull the ID from session
    - Using prisma, update the user's info using the new form data + the id from session.
    
    Next Steps:
    - Redirect user after submit to Dashboard page (not created yet)

--- pages/api/profile/index.js
+++ pages/api/profile/index.js
@@ -1,52 +1,35 @@
 import { prisma } from "@/utils/db";
 import { getSession } from "next-auth/react";
 
-export default async (req, res) => {
+export default async function handle(req, res) {
   const session = await getSession({
     req,
   });
-
   const userId = session.user.id;
 
-  if (!session) {
-    res.status(401).json({
-      error: "Unauthenticated user",
-    });
-  } else {
-    res.status(200).json({
-      message: "Success",
-      userId,
-    });
-  }
-};
+  const { name, instagram, number } = req.body;
+  console.log(name, instagram, number);
 
-// // POST /api/profile
-// // Required fields in body: title
-// // Optional fields in body: content
-// export default async function Handle(req, res) {
-//   const session = await unstable_getServerSession(req, res);
-//   if (session) {
-//     // Signed in
-//     console.log("Session", JSON.stringify(session, null, 2));
-//   } else {
-//     // Not Signed in
-//     res.status(401);
-//   }
-//   res.end();
+  const result = await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      name,
+      instagram,
+      number,
+    },
+  });
+  res.json(result);
 
-//   // const { userId } = session;
-//   // console.log(userId);
-//   // const result = await prisma.post.update({
-//   //   where: {
-//   //     email:
-//   //   }
-//   //   data: {
-//   //     name,
-//   //     instagram,
-//   //     number,
-//   //   },
-//   // });
-//   // res.json(result);
-//   // res.send("Works");
-//   // res.status(200).json(req.body);
-// }
+  // if (!session) {
+  //   res.status(401).json({
+  //     error: "Unauthenticated user",
+  //   });
+  // } else {
+  //   res.status(200).json({
+  //     message: "Success",
+  //     userId,
+  //   });
+  // }
+}
