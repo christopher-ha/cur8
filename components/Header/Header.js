92997ee5693eaee0c22cdea3fc14cdc0daa02293
bhris <contact@bhris.digital>
2022-07-06 11:14:54 -0400

    feat: Add header menu on mobile
    Still not perfect and needs different pathing (asPath, basePath) depending on what screen its being rendered on, and also on dashboard none of the menu items are relevant since theres no active campaign selected.
    
    Possibly add a links to "root" directories of moodboards that shows from all campaigns? Not sure how this would work with things that don't have directories like styled looks or call sheet -- we would require user to select a campaign again essentially.
    
    My other solution was to render an edit profile and logout button.
    
    Fixes
    - Adjusted dropzone to be 94vh to allow room for the heading to be clicked (no click zone). Also users will most likely not drag an image to upload inside of the header, so this works fine.
    
    Resources
    - react-modal root directory in Next.js: https://www.reddit.com/r/nextjs/comments/q65otv/reactmodal_in_nextjs/
    - Remove default styling on react-modal: http://reactcommunity.org/react-modal/styles/

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,7 +1,46 @@
 import styles from "@/components/Header/Header.module.scss";
 import Link from "next/link";
+import Modal from "react-modal";
+import { useState } from "react";
+import Menu from "@/components/Menu/Menu";
+
+const modalStyle = {
+  overlay: {},
+  content: {
+    display: "flex",
+    "justify-content": "center",
+    "align-items": "center",
+    "flex-direction": "column",
+    position: "absolute",
+    top: "0px",
+    left: "0px",
+    right: "0px",
+    bottom: "0px",
+    background: "#fff",
+    overflow: "auto",
+    WebkitOverflowScrolling: "touch",
+    padding: "1rem",
+    border: "none",
+  },
+};
+
+Modal.setAppElement("#__next");
 
 export default function Header({ title, campaign, logoVisible }) {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className={styles.header}>
       <Link href="/">
@@ -16,6 +55,25 @@ export default function Header({ title, campaign, logoVisible }) {
       <h3 className={styles.header__title}>{title}</h3>
       <h3 className={styles.header__campaign}>{campaign}</h3>
       <h3 className={styles.header__menu}>MENU</h3>
+      <div className={styles.header__menu__container}>
+        <img
+          className={styles.header__menu__mobile}
+          src="/icons/hamburger-menu.svg"
+          alt="Hamburger Menu"
+          onClick={openModal}
+        ></img>
+      </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={modalStyle}
+        contentLabel="Hamburger Menu"
+      >
+        <Menu />
+        <button onClick={closeModal}>close</button>
+      </Modal>
     </div>
   );
 }
--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -3,10 +3,19 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Menu({ dashboardVisible }) {
-  const { asPath } = useRouter();
+  const { asPath, basePath } = useRouter();
   console.log(asPath);
   return (
     <div className={styles.menu}>
+      <Link href={`${basePath}/campaigns`}>
+        <h1
+          className={`${styles.menu__item} ${
+            dashboardVisible === false ? `${styles.hidden}` : ""
+          }`}
+        >
+          Dashboard
+        </h1>
+      </Link>
       <Link href={`${asPath}/moodboards`}>
         <h1 className={styles.menu__item}>Moodboards</h1>
       </Link>
