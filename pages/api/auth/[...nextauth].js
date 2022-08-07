d2b9fbd9a914522463c281f43d8628f440824d1d
bhris <contact@bhris.digital>
2022-08-07 16:33:32 -0400

    fix: Restore default error page for NextAuth
    When a user would click the same login link twice, it would render an error page but in raw HTML
    Deleted the files and pathing for error.js to restore default behaviour.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -28,7 +28,6 @@ export default NextAuth({
   pages: {
     signIn: "/auth/login",
     signOut: "/auth/logout",
-    error: "/auth/error", // Error code passed in query string as ?error=
     verifyRequest: "/auth/verify-request", // (used for check email message)
     newUser: "/auth/profile", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
